ac3c35751cec1aac28755359fd01683537fe6fc0
IlyaZadyabin <z.ilya.007@gmail.com>
2021-10-06 17:29:05 +0300

    Initial

---
+++ ObjectRecognition/Model.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.ML;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using ObjectRecognition.DataStructures;
+using static Microsoft.ML.Transforms.Image.ImageResizingEstimator;
+
+namespace ObjectRecognition
+{
+    public class Model
+    {
+        static readonly string[] classesNames = new string[] { "person", "bicycle", "car", "motorbike",
+            "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+            "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear",
+            "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+            "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+            "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+            "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa",
+            "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard",
+            "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+            "teddy bear", "hair drier", "toothbrush" };
+
+        public EnginePool enginePool;
+
+        public Model()
+        {
+            var modelPath = Path.Combine(GetAbsolutePath(@"../../../Assets"), "Models", "yolov4.onnx");
+
+            MLContext mlContext = new MLContext();
+
+            var pipeline = mlContext.Transforms.ResizeImages(inputColumnName: "bitmap", outputColumnName: "input_1:0", imageWidth: 416, imageHeight: 416, resizing: ResizingKind.IsoPad)
+                .Append(mlContext.Transforms.ExtractPixels(outputColumnName: "input_1:0", scaleImage: 1f / 255f, interleavePixelColors: true))
+                .Append(mlContext.Transforms.ApplyOnnxModel(
+                    shapeDictionary: new Dictionary<string, int[]>()
+                    {
+                        { "input_1:0", new[] { 1, 416, 416, 3 } },
+                        { "Identity:0", new[] { 1, 52, 52, 3, 85 } },
+                        { "Identity_1:0", new[] { 1, 26, 26, 3, 85 } },
+                        { "Identity_2:0", new[] { 1, 13, 13, 3, 85 } },
+                    },
+                    inputColumnNames: new[]
+                    {
+                        "input_1:0"
+                    },
+                    outputColumnNames: new[]
+                    {
+                        "Identity:0",
+                        "Identity_1:0",
+                        "Identity_2:0"
+                    },
+                    modelFile: modelPath, recursionLimit: 100));
+
+            var model = pipeline.Fit(mlContext.Data.LoadFromEnumerable(new List<YoloV4BitmapData>()));
+            enginePool = new EnginePool(mlContext, model);
+        }
+
+        public IReadOnlyList<YoloV4Result> predictionResults(Bitmap bitmap)  {
+            var engine = enginePool.Get();
+            enginePool.Release(engine);
+            var predict = engine.Predict(new YoloV4BitmapData() { Image = bitmap });
+            return predict.GetResults(classesNames, 0.3f, 0.7f);
+        }
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+
+    }
+
+}
---
+++ ObjectRecognition/ObjectDraw.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using ObjectRecognition.DataStructures;
+
+namespace ObjectRecognition
+{
+    public class ObjectRectangles
+    {
+        const string imageOutputFolder = @"Assets\Output";
+     
+        public static void Draw(Bitmap bitmap, IReadOnlyList<YoloV4Result> results, string imageName)
+        {
+            var g = Graphics.FromImage(bitmap);
+
+            foreach (var res in results)
+            {
+                var x1 = res.BBox[0];
+                var y1 = res.BBox[1];
+                var x2 = res.BBox[2];
+                var y2 = res.BBox[3];
+                g.DrawRectangle(Pens.Red, x1, y1, x2 - x1, y2 - y1);
+                g.FillRectangle(new SolidBrush(Color.FromArgb(50, Color.Red)), x1, y1, x2 - x1, y2 - y1);
+                g.DrawString(res.Label + " " + res.Confidence.ToString("0.00"),
+                             new Font("Arial", 12), Brushes.Blue, new PointF(x1, y1));
+            }
+
+            bitmap.Save(Path.Combine(imageOutputFolder, Path.ChangeExtension(imageName, "_processed" + Path.GetExtension(imageName))));
+        }
+    }
+}
