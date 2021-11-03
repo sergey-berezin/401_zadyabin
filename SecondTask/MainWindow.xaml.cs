65927db18396e21052c8beab6e45b6123c473f73
IlyaZadyabin <z.ilya.007@gmail.com>
2021-11-03 23:44:14 +0300

    Choose folder

---
+++ SecondTask/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecondTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Forms.DialogResult observedDirectory;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                var result = dialog.ShowDialog();
+
+                if (result.ToString() != string.Empty)
+                {
+                    PathTextBlock.Text = dialog.SelectedPath;
+                }
+            }
+        }
+    }
+}
